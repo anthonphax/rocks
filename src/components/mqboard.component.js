d5d44ff6929940e619c65aa1e4e2b7034d1fbfa0
anthonphax <anthon_hax>
2023-06-15 00:09:15 -0300

    Atualização dashboard

--- src/components/mqboard.component.js
+++ src/components/mqboard.component.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react'
-
+import usinas from "../data/usina.json";
 
 export default class Mqboard extends Component {
+    getlocation = (button) => {
+        console.log("apertado")
+    }
+    move = () => {
+                console.log("botão pressionado")
+                };
     render() {
         return (
-            
-            <><button>
-                <span>USIPAV</span>
-            </button>
-                <form className='dashboard'>
+            <> 
+                <div className='option-div'>
+                <h1 className='option-title'>Escolha uma Usina</h1>
+                
+                    {usinas.usinas.map((usinas) => {
+                        return <button 
+                                onClick={() => {console.log(usinas.cidade)}} 
+                                key={usinas.id} 
+                                value={usinas.nome} 
+                                className='option-plant'>
+                                    {usinas.nome}
+                                </button>
+                    })}
+                    <form className='dashboard'>
+                        
+                    </form>
+
                     
-                </form></>
+
+                </div> 
+
+                
+            </>
         )
     }
 }
