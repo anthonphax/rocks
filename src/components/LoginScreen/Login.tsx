c38017c35174abd65145f843ac81942f318fa6fd
Pedro Delgado Henriques <pedrodelgadohenriques1@gmail.com>
2023-10-27 22:11:30 -0300

    continuando login

--- src/components/LoginScreen/Login.tsx
+++ src/components/LoginScreen/Login.tsx
@@ -52,11 +52,43 @@ const data = {
 
 const Login = () => {
     return (
-        <Box marginY={3}>
-            <Card elevation={5} sx={{ padding: 2 }}>
-                <Line options={options} data={data} />
-            </Card>
-        </Box>
+    <div className="Auth-form-container">
+        <form className="Auth-form">
+        <div className="Auth-form-content">
+          <h3 className="Auth-form-title">Sign In</h3>
+          <div className="form-fields">
+            <div className="form-group mt-3">
+                <label>Email address</label>
+                <input
+                type="email"
+                className="form-control mt-1"
+                placeholder="Enter email"
+                />
+            </div>
+            <div className="form-group mt-3">
+                <label>Password</label>
+                <input
+                type="password"
+                className="form-control mt-1"
+                placeholder="Enter password"
+                />
+            </div>
+            <p className="forgot-password text-right mt-2">
+            Forgot <a className="esqueceu" href="#"> password</a>
+          </p>
+          </div>
+          
+          <div className="d-grid gap-2 mt-3">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+          
+        </div>
+      </form>
+     
+      
+    </div>
     )
 }
 
